c6e5d329bb356072f83549aea2d45862ad350910
e1bandito <aleksandr.shitov@mail.ru>
2020-11-02 10:28:13 +0300

    Update 2020-11-02T07:28:03.600Z

--- src/js/import/main.js
+++
@@ -1,4 +0,0 @@
-import {} from '../../blocks/burger/burger';
-import {} from '../../blocks/filter/filter';
-import {} from '../../blocks/range/range';
-import {} from '../../blocks/product/product';
